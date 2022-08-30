0b716433134828acf6fc0971496f6d51ce056f9a
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2022-08-30 14:20:43 -0700

    additional files created

---
+++ script.js
@@ -0,0 +1,33 @@
+// declare variables
+
+function addToCart() {   
+    let item1 = document.querySelector('.item1').textContent;
+    let item2 = document.querySelector('.item2').textContent;
+
+    for(let i = 0; i < items.length; i++) {
+        console.log(items[i].textContent)
+    }
+    // console.log(item);
+    // // let item1 = document.getElementById('item1').textContent;
+    // // console.log(item1);
+    // document.getElementById('cart-container--items').innerText = item;
+
+    // document.getElementById('')
+
+}
+
+function removeFrontCart() {
+
+}
+
+function editItem() {
+
+}
+
+function clearCart() {
+
+}
+
+
+
+
---
+++ src/Data.js
@@ -0,0 +1,86 @@
+let shoeData = [
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+    {
+        id: "",
+        img: "",
+        name: "",
+        description: "",
+        price: ""
+    },
+]
